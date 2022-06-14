04893fd354070696ef58ea6f26e4c43f276b6bd7
Seagull <101097344+Canned-Seagull23@users.noreply.github.com>
2022-06-14 15:41:56 +0800

    Changed the promise layout
    - Used async await instead of .then

--- bin/dsc.ts
+++ bin/dsc.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 "use strict";
 
+import { rejects } from "assert";
+import { allowedNodeEnvironmentFlags } from "process";
+
 const args = process.argv.slice(2);
 
 const fs = require('fs');
@@ -10,16 +13,20 @@ if (RegExp("^C:[/\\\\]").test(args[0])) p = args[0];
 else if (args[0] === '') console.log("A file path must be provided!");
 else p = path.join(__dirname, '../', args[0]);
 const filepath: string = p;
-let file: String;
-let read = new Promise<void>((resolve) => {
-    fs.readFile(filepath, "UTF8", function (err, data) {
-        if (err) {
-            console.log("An error occurred");
-            console.log(err)
-        }
-        file = data;
-        resolve();
-    });
-}).then(() => {
-      console.log(file);
-  });
+let file: any;
+function readfile() {
+    return new Promise<String>((resolve, reject) => {
+        fs.readFile(filepath, "UTF8", function (err, data) {
+            if (err) {
+                console.log("An error occurred");
+                console.error(err)
+                reject(err);
+            }
+            resolve(data);
+        });
+    })
+}
+(async () => {
+    file = await readfile();
+    console.log(file)
+})();
