095f0cb6542fd972983e7378ae69f3ca61b79033
Seagull <101097344+Canned-Seagull23@users.noreply.github.com>
2022-06-14 12:49:55 +0800

    Fixed the file input system
    - Used a promise on the readfile function
    - Fixed some bugs on the relative path support

--- bin/dsc.js
+++ bin/dsc.js
@@ -4,17 +4,23 @@ var args = process.argv.slice(2);
 var fs = require('fs');
 var path = require('path');
 var p;
-if (RegExp("^C:/").test(args[0]))
+if (RegExp("^C:[/\\\\]").test(args[0]))
     p = args[0];
+else if (args[0] === '')
+    console.log("A file path must be provided!");
 else
     p = path.join(__dirname, '../', args[0]);
 var filepath = p;
-fs.readFile(filepath, "UTF8", function (err, data) {
-    if (err) {
-        console.log("An error occurred");
-    }
-    file = data;
-});
 var file;
-console.log(file);
-console.log(filepath)
+var read = new Promise(function (resolve) {
+    fs.readFile(filepath, "UTF8", function (err, data) {
+        if (err) {
+            console.log("An error occurred");
+            console.log(err);
+        }
+        file = data;
+        resolve();
+    });
+}).then(function () {
+    console.log(file);
+});
--- bin/dsc.ts
+++ bin/dsc.ts
@@ -6,14 +6,20 @@ const args = process.argv.slice(2);
 const fs = require('fs');
 const path = require('path');
 let p: string;
-if (RegExp("^C:/").test(args[0])) p = args[0];
+if (RegExp("^C:[/\\\\]").test(args[0])) p = args[0];
+else if (args[0] === '') console.log("A file path must be provided!");
 else p = path.join(__dirname, '../', args[0]);
 const filepath: string = p;
-fs.readFile(filepath, "UTF8", (err, data) => {
-    if (err) {
-        console.log(`An error occurred`);
-    }
-    file = data;
-})
 let file: String;
-console.log(file)
+let read = new Promise<void>((resolve) => {
+    fs.readFile(filepath, "UTF8", function (err, data) {
+        if (err) {
+            console.log("An error occurred");
+            console.log(err)
+        }
+        file = data;
+        resolve();
+    });
+}).then(() => {
+      console.log(file);
+  });
